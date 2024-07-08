61eed4749406621274872fef1b5e6239ce20615c
Sam-Cowman <sam.p.cowman@gmail.com>
2024-07-08 11:07:07 -0500

    Added comments to the code

--- models/Tag.js
+++ models/Tag.js
@@ -6,13 +6,28 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Define the 'id' attribute with properties
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Define the 'tag_name' attribute with properties
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
+    // Provide the sequelize instance
     sequelize,
+    // Disable automatic timestamps (createdAt, updatedAt)
     timestamps: false,
+    // Prevent sequelize from renaming the table
     freezeTableName: true,
+    // Use underscored format for automatically added attributes (e.g., created_at)
     underscored: true,
+    // Set the model name to 'tag'
     modelName: 'tag',
   }
 );
